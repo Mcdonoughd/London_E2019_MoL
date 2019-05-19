50bd32a86cafe211a4bd470cfd7164acc8d2adc7
Daniel McDonough <dannmcdonough@gmail.com>
2019-05-19 23:39:36 +0100

    da

--- VictorianWalk/ViewController.swift
+++ VictorianWalk/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let BG_Image = UIImageView(image: UIImage(named: "London_Street"), highlightedImage: nil)
+        BG_Image.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        BG_Image.alpha = 0.8
+        self.navigationController?.view.addSubview(BG_Image)
+        
+        
     }
 
 
