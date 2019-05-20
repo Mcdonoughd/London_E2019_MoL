9b6cab1e57bbb12008512bce51a5347462a8701d
Daniel McDonough <dannmcdonough@gmail.com>
2019-05-20 03:34:09 +0100

    Added basic Tut screen

---
+++ VictorianWalk/TutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TutViewController.swift
+//  VictorianWalk
+//
+//  Created by Daniel McDonough on 20/05/2019.
+//  Copyright © 2019 Daniel McDonough. All rights reserved.
+//
+
+import UIKit
+
+class TutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
+        return .landscapeLeft
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    
+}
+
---
+++ VictorianWalk/Tutorial.swift
@@ -0,0 +1,13 @@
+//
+//  Tutorial.swift
+//  VictorianWalk
+//
+//  Created by Daniel McDonough on 20/05/2019.
+//  Copyright © 2019 Daniel McDonough. All rights reserved.
+//
+
+import UIKit
+
+class Tutorial: NSObject {
+
+}
