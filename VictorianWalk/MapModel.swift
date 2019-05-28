4d5befdd0f8a0892ad955614e02cb0af7da40d6d
Daniel McDonough <dannmcdonough@gmail.com>
2019-05-28 03:49:44 +0100

    Added activity data

--- VictorianWalk/MapModel.swift
+++ VictorianWalk/MapModel.swift
@@ -24,7 +24,7 @@ class MapModel{
         
         "3":Booth(id:3, desc:"A barber’s services for men included haircuts, shaves, and shampooing. Hair was also, ‘signed.’ A lit wax taper was passed over the ends and the burnt tips rubbed off to seal and strengthen the hair. Barbers often stored customer’s personal shaving mugs at the back of their shop. They also sold tobacco and pipes’",
             title:"Barber Shop",
-            Activities: [.none]),
+            Activities: [.Act, .Imagine]),
         
         "4":Booth(id:4, desc:"Working men in London relaxed in the ‘pub’. It was the heart of the local community. They met with friends, played darts and attended political meetings. Children sat outside, awaiting their parents, or were sent in to buy beer for drinking at home. Many pubs had a public bar and a saloon. Saloons were favored by couples as they were more comfortable.",
             title:"The Pub",
@@ -40,11 +40,11 @@ class MapModel{
         
         "7":Booth(id:7, desc:"Goods from around the world arrived at London’s ports. They were taken from the docks to the city’s many warehouses. Tea was imported from China, India, and Ceylon(now called Sri Lanka); cocoa and coffee from Africa and South America. After being graded and weighed, tea was blended, and coffee roasted and ground for distribution to grocers, tea rooms and hotels.",
             title:"Tea & Coffee Warehouse",
-            Activities: [.none]),
+            Activities: [.Imagine]),
         
         "8":Booth(id:8, desc:"Mr Elkington on Lamb’s Conduit Street made visiting cards, business cards, invitations, and bookplates. He might spend half a day engraving one intricate design onto a copper plate for printing. Engravers usually worked alone in small workshops that smelt strongly of the inks, acids, and polishes they used. They received work from private customers and small printing firms",
             title:"Engraver",
-            Activities: [.none]),
+            Activities: [.Imagine]),
         
         "9":Booth(id:9, desc:"London’s watchmakers produced expensive watches for sale in the West End and around the world. Most were based in Clerkenwell. Their intricate work required strong light, so they often placed their workbenches near a window, The trade was organized on an ‘outwork’ system. Watch parts moved between the homes and workshops of many workers before being assembled by ‘finishers’. Specialist skills included hinge-making, dial-painting, and case-making.",
             title:"Watchmaker",
@@ -60,7 +60,7 @@ class MapModel{
         
         "12":Booth(id:12, desc:"Grocers liked Fred Bugg in Bayswater sold the essentials of daily life. Tea, flour, sugar, and rice were drawn from larger containers, then weighed and wrapped for customers. Grocers also sold firewood and paraffin and for the first time, canned and processed foods - condensed milk, tinned fish. They often lived about the shops. Grocers worked weekdays and late into the evening on Saturdays, when workers came to shop after being paid their wages.",
             title:"Grocer",
-            Activities: [.none]),
+            Activities: [.Imagine]),
         
         "13":Booth(id:13, desc:"For those too poor to pay the doctor, the pharmacist prepared pills and powder, or sold pre-packaged ‘patent’ remedies. The herbs and chemicals he mixed together were stored in tiny drawers behind the counter. Poisonous liquids were kept in dark green or blue glass bottles with fluted sides to distinguish them from harmless fluids.",
             title:"Pharmacy",
@@ -79,7 +79,7 @@ class MapModel{
         
         "17":Booth(id:17,desc:"Pawnbrokers, referred to as ‘uncle’, were the poor person’s bank. Clothing and jewellery were handed over in exchange for a loan. If the loan was not repaid within a year, ‘uncle’ could sell the goods. By 1900 there were 700 pawnbrokers in London. Forfeited articles for sale in the window include Sunday best clothing, wedding gifts and ornaments.",
             title:"Pawnbroker",
-            Activities: [.none]),
+            Activities: [.Search,.Imagine]),
         
         "18":Booth(id:18,desc:"London’s financial services employed thousands of clerks. Their duties included bookkeeping, correspondence and cashier work behind grilles or screens. Banking jobs offered security, promotion and generally higher wages. In the 1890s the Bank of England for the first time hired women who had passed an exam to count and register banknotes. These offices were created from the central banking hall and offices of Barings Bank in Bishopsgate.",
             title:"Bank Clerks' Office",
@@ -87,7 +87,7 @@ class MapModel{
         
         "19":Booth(id:19,desc:"Bank managers were highly respected members of the community. Bank accounts were primarily for businesses and the rich. The poor were paid weekly in cash that barely lasted seven days. Small savings for Christmas or weddings could be placed in a Post Office Savings or National Penny bank. They accepted deposits from a penny upwards.",
             title:"Bank Manager's Office",
-            Activities: [.Imagine])
+            Activities: [.Imagine,.Quiz])
     ]
     
   let errorBooth = Booth(id:0, desc:"Do not panic!!! The devlopers of this application have not accounted for this to happen. We have trained monkey's working on fixing it right now.            ",title:"404",Activities: [.none])
