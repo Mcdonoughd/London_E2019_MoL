fc52161c5bc35f46811ea7f488b23f3953d0926a
Mario <macastro2@wpi.edu>
2019-05-31 12:07:30 +0100

    pub game

---
+++ VictorianWalk/pubGame.swift
@@ -0,0 +1,47 @@
+//
+//  pubGame.swift
+//  VictorianWalk
+//
+//  Created by Museum of London on 31/05/2019.
+//  Copyright © 2019 Daniel McDonough. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class pubGame{
+    
+    var turns:[Int]
+    
+    var player1ScoredInBars:[Int]
+    var player2ScoredInBars:[Int]
+
+    
+    var player1Pennys:[SKNode]
+    var player2Pennys:[SKNode]
+    
+    init(){
+        
+        var turn = 1;
+        
+        player1ScoredInBars = [Int]()
+        player1Pennys = [SKNode]()
+        
+        player2ScoredInBars = [Int]()
+        player2Pennys = [SKNode]()
+        
+        for _ in 0...9 {
+            self.player1ScoredInBars.append(0);
+            self.player2ScoredInBars.append(0);
+        }
+    }
+    
+    
+}
+
+class player{
+    var playerScoredInBars:[Int]
+    var playerPennies:[SKNode]
+}
+
+
