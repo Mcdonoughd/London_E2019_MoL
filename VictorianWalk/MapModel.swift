48a212a21e5cc6b10d5b2c795c7ecded892b6473
Mario <macastro2@wpi.edu>
2019-06-05 12:30:11 +0100

    fixed typo

--- VictorianWalk/MapModel.swift
+++ VictorianWalk/MapModel.swift
@@ -14,25 +14,25 @@ class MapModel{
     
     
     var TitleDesc:[String : Booth] = [
-        "1":Booth(id:1, desc:"Victorian children saved their farthings to buy ”penny toys” from street sellers and dazzling toy arcades such as Lowther’s on the Strand. Lowther’s was an ”Aladdin fairy palace” of expensive train sets, dolls, and lead soldiers.",
+        "1":Booth(id:1, desc:"Victorian children saved their farthings to buy \"penny toys\" from street sellers and dazzling toy arcades such as Lowther’s on the Strand. Lowther’s was an \"Aladdin fairy palace\" of expensive train sets, dolls, and lead soldiers.",
             title:"Toy Shop",hasActivity: true),
         
         "2":Booth(id:2, desc:"Tobacconists sold blends of loose tobacco, snuff, cigarettes, and smoking accessories. During Victorian times, habits were changing. Rolled cigarettes were available, and briar-root pipes replaced clay ones.",
             title:"Tobacconist"),
         
-        "3":Booth(id:3, desc:"Victorian men would go to the barber’s for haircuts, shaves, shampooing, and ”singeing,” a process involving burning hairs to seal them off and strengthen them. Barbers often stored customers’ personal shaving mugs at the back of their shop.",
+        "3":Booth(id:3, desc:"Victorian men would go to the barber’s for haircuts, shaves, shampooing, and \"singeing,\" a process involving burning hairs to seal them off and strengthen them. Barbers often stored customers’ personal shaving mugs at the back of their shop.",
                   title:"Barber Shop",hasActivity:true),
         
         "4":Booth(id:4, desc:"Working men would relax in the pub, meet with friends, play darts, or attend political meetings. Children sat outside, awaiting their parents, or were sent in to buy beer for drinking at home.",
             title:"The Pub", hasActivity: false),
         
-        "5":Booth(id:5, desc:"James Powell & Sons’ showroom was attached to their glasshouse off Fleet Street in Whitefriars. Their table and decorative glass were famed for their purity, subtle colours, and delicate decoration. The ‘Eve’ mosaic is a copy of one Powell’s made for St. Paul’s Cathedral.",
+        "5":Booth(id:5, desc:"James Powell & Sons’ showroom was attached to their glasshouse off Fleet Street in Whitefriars. Their table and decorative glass were famed for their purity, subtle colours, and delicate decoration. The \"Eve\" mosaic is a copy of one Powell’s made for St. Paul’s Cathedral.",
             title:"Glass Showroom",hasActivity: true),
 
         "6":Booth(id:6, desc:"Introduced by Italian immigrants, barrel organs could be programmed to play up to 12 popular tunes of the day. By 1900 there were nearly 500 in London’s noisy streets, competing with bagpipe players, singers, clowns, performing monkeys, and knife swallowers.",
             title:"Barrel Organ"),
         
-        "7":Booth(id:7, desc:"Tea from China, India, and Ceylon (now called Sri Lanka) arriving in London was taken from the docks to the city’s many warehouses. After being graded and weighed, tea was blended and coffee was roasted and ground for distribution to grocers, tea rooms and hotels. ",title:"Tea & Coffee Warehouse",hasActivity: true),
+        "7":Booth(id:7, desc:"Tea from China, India, and Ceylon (now called Sri Lanka) arriving in London was taken from the docks to the city’s many warehouses. After being graded and weighed, tea was blended and coffee was roasted and ground for distribution to grocers, tea rooms, and hotels.",title:"Tea & Coffee Warehouse",hasActivity: true),
         
         "8":Booth(id:8, desc:"Mr. Elkington on Lamb’s Conduit Street made visiting cards, business cards, invitations, and bookplates. Engravers usually worked alone in small workshops that smelt strongly of the inks, acids, and polishes they used.",
             title:"Engraver",hasActivity: true),
@@ -55,12 +55,13 @@ class MapModel{
         "14":Booth(id:14, desc:"Respectable women in Victorian London always covered their heads outdoors. At the milliner’s they bought hats, bonnets, and caps, as well as various decorations. Milliners would clean and redesign hats to suit the latest trends of Paris.",
             title:"Milliner"),
         
-        "15":Booth(id:15,desc:"Tailors catered to “white-collar” office workers. Clients first selected a length of material. The tailor measured them, prepared an paper pattern, and cut the cloth. He gave the pieces to an ‘outworker’ or assistant to stitch together on a sewing machine.",
+        "15":Booth(id:15,desc:"Tailors catered to \"white-collar\" office workers. Clients first selected a length of material. The tailor measured them, prepared a paper pattern, and cut the cloth. He gave the pieces to an \"outworker\" or assistant to stitch together on a sewing machine.",
             title:"Tailor"),
-        "16":Booth(id:16,desc:"Many stationers manufactured popular “fancy stationery” - handmade Christmas and greeting cards, as well as Valentine's cards. Their shops were like newsagents today. Daily papers were widely read in London, especially the Daily Telegraph and the Daily Mail.",
+        
+        "16":Booth(id:16,desc:"Many stationers manufactured popular \"fancy stationery\" - handmade Christmas and greeting cards, as well as Valentine's cards. Their shops were like newsagents today. Daily papers were widely read in London, especially the Daily Telegraph and the Daily Mail.",
             title:"Fancy Stationer"),
         
-        "17":Booth(id:17,desc:"Pawnbrokers, referred to as “uncle,” were the poor person’s bank. Clothing and jewellery were handed over in exchange for a loan. If the loan was not repaid within a year, “uncle” could sell the goods. By 1900 there were 700 pawnbrokers in London.",
+        "17":Booth(id:17,desc:"Pawnbrokers, referred to as \"uncle,\" were the poor person’s bank. Clothing and jewellery were handed over in exchange for a loan. If the loan was not repaid within a year, \"uncle\" could sell the goods. By 1900 there were 700 pawnbrokers in London.",
             title:"Pawnbroker",hasActivity: true),
         
         "18":Booth(id:18,desc:"London’s financial services employed thousands of clerks who did bookkeeping, correspondence, and cashier work behind grilles or screens. In the 1890s the Bank of England for the first time hired women who had passed an exam to count and register banknotes.",
@@ -70,7 +71,7 @@ class MapModel{
             title:"Bank Manager's Office",hasActivity: true)
     ]
     
-  let errorBooth = Booth(id:0, desc:"Do not panic!!! The developers of this application have not accounted for this to happen. We have trained monkeys working on fixing it right now.            ",title:"404")
+  let errorBooth = Booth(id:0, desc:"Do not panic!!! The developers of this application have not accounted for this to happen. We have trained monkeys working on fixing it right now.",title:"404")
     
     
     func chooseBooth(at: String) -> Booth
