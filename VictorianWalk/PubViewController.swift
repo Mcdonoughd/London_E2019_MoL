6abc72d7a13f0cd728a2b1384838755cb7b30d06
Daniel McDonough <dannmcdonough@gmail.com>
2019-06-17 15:56:48 +0100

    fixed flipping on the shoveHapenny

--- VictorianWalk/PubViewController.swift
+++ VictorianWalk/PubViewController.swift
@@ -93,14 +93,13 @@ class PubViewController: UIViewController {
     }
     
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    //force landscape mode
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
+        return .landscapeLeft
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
     
 }
